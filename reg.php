0656fc61370a0c96386dfc702c018a1724320794
colibri-vitja <59998040+colibri-vitja@users.noreply.github.com>
2020-01-25 12:29:58 +0200

    new

--- reg.php
+++ reg.php
@@ -1,12 +1,18 @@
 <?php
-require 'vendor/autoload.php';
+session_start();
+$_SESSION["test"] = "1234567890"
+require 'vendor/autoload.php';'
 require "conection.php";
-$app = new \atk4\ui\App("kulok");
-$app->initLayout("Centered");
+$app = new \atk4\ui\App("kulok");'
+$app->initLay[out("Centered");
 $form  = $app->layout->add("Form");
 $form->setModel(new User($db));
 $form->buttonSave->set("Create Account");
-
+$model = new User($db);
 $form->onSubmit(function($from){
-return new \atk4\ui\jsExpression('document.location = "main.php"');
+  $nickname = $form->model['$nickname']
+  $form->model->save();
+  $model->tryLoadBy('nickname',$nickname);
+  $_SESSION["user_id"] = $model->id;
+  return new \atk4\ui\jsExpression('document.location = "main.php"');
 });
